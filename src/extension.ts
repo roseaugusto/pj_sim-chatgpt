840bc9086ef722039e0ddc9b36a17556d83c2860
Brucenigel <brucenige.vilo@sun-asterisk.com>
2023-06-09 13:20:58 +0800

    Dropdown UI

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { SidebarProvider } from './providers/SidebarProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -14,17 +13,8 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		const editor = vscode.window.activeTextEditor;
-
-		const sidebarProvider = new SidebarProvider(context.extensionUri);
-											vscode.window.registerWebviewViewProvider(
-												'sim-chatgpt-sidebar',
-												sidebarProvider
-											);
-	
 		vscode.window.showInformationMessage('Hello World from Programming Language Translator!');
 	
 	context.subscriptions.push();
